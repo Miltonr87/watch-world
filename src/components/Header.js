8808525233b13f70c06472cf81e9b33ffaf4271f
Milton Rodrigues <miltonrodrigues713@gmail.com>
2021-10-06 23:01:37 -0300

    Added Modal on SignIn and Signup

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import Login from './buttons/Login';
+import Signup from './buttons/Signup';
 
 const Header = () => {
     return (
         <>
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
                 <div className="container-fluid py-2">
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <button className="navbar-toggler" type="btn btn-outlined-primary" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
@@ -26,6 +28,8 @@ const Header = () => {
                         </ul>
                     </div>
                     <NavLink className="navbar-brand mx-auto fw-bold" to="/">Watch World</NavLink>
+                    <Login />
+                    <Signup />
                 </div>
             </nav>
         </>
