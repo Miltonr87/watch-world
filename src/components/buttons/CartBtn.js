c53494d4c17f23145c0cb13f494ccd6e80611a95
Milton Rodrigues <miltonrodrigues713@gmail.com>
2021-10-14 22:16:21 -0300

    Added Carousel and Eslint style

--- src/components/buttons/CartBtn.js
+++ src/components/buttons/CartBtn.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React from "react";
+import { NavLink } from "react-router-dom";
 
 const CartBtn = () => {
-    return (
-        <div>
-
-        </div>
-    )
+  return (
+    <>
+      <NavLink to="/cart" className="btn btn-outline-primary ms-2">
+        <span className="fa fa-shopping-cart me-1"> Cart(0) </span>
+      </NavLink>
+    </>
+  );
 };
 
 export default CartBtn;
